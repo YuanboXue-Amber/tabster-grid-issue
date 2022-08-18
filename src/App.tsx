7f107b2a19931aa2e94442175492f5120e83a873
YuanboXue-Amber <yuanboxue@microsoft.com>
2022-08-18 23:12:19 +0200

    enhance example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Button as V0Button, Popup } from "@fluentui/react-northstar";
-import { Button } from "@fluentui/react-components";
 import { useArrowNavigationGroup } from "@fluentui/react-tabster";
 import * as React from "react";
 import { Grid } from "./Grid";
@@ -13,12 +12,12 @@ const Content = () => {
   return (
     <>
       {/* this button is important */}
-      <V0Button>V0Button outside of grid</V0Button>
+      <button>button outside of grid</button>
       {/* this div is important */}
       <div>
         <Grid {...arrowKeyNavigationAttributes} role='menu' columns={3}>
           {new Array(9).fill(0).map((i, idx) => (
-            <Button key={idx}>{`grid-item-${idx}`}</Button>
+            <button key={idx}>{`grid-item-${idx}`}</button>
           ))}
         </Grid>
       </div>
